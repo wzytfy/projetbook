7cf3d12093a8d2d7733e16fb0a7c31f7c7bce87a
Isaac deeming <isaacdeeming92@gmail.com>
2023-04-22 14:53:22 +1200

    "i think im getting git a little"

--- pythonbook.py
+++ pythonbook.py
@@ -1,4 +1,4 @@
-age = 70
+age = 100
 if age == 15:
     print("im a 15 years of age")
 elif age == 20:
@@ -9,3 +9,6 @@ elif age  == 70:
     print("i am 70 years and 7 cm in")
 elif age == 100:
     print("were is my letter mis qween i am 100")
+    print("i still love mum")
+elif age == 88: 
+    print("i think im ready to go")
