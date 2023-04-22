5cf52784e7d7b3d943e0e3ef95d8fb79d6b57676
Isaac deeming <isaacdeeming92@gmail.com>
2023-04-22 14:26:39 +1200

    "fucking arund"

--- pythonbook.py
+++ pythonbook.py
@@ -1,7 +1,11 @@
-age = 34
+age = 70
 if age == 15:
     print("im a 15 years of age")
 elif age == 20:
     print("i am 20 years of age")
 elif age == 34:
     print("i am 34 years of age")
+elif age  == 70:
+    print("i am 70 years and 7 cm in")
+elif age == 100:
+    print("were is my letter mis qween i am 100")
