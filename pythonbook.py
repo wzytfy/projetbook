fe3b60564ce8700a1db34e307586c06772ce9775
Isaac deeming <isaacdeeming92@gmail.com>
2023-04-24 17:21:11 +1200

    "useing this repository to lern git"

--- pythonbook.py
+++ pythonbook.py
@@ -1,14 +1,9 @@
-age = 100
-if age == 15:
-    print("im a 15 years of age")
-elif age == 20:
-    print("i am 20 years of age")
-elif age == 34:
-    print("i am 34 years of age")
-elif age  == 70:
-    print("i am 70 years and 7 cm in")
-elif age == 100:
-    print("were is my letter mis qween i am 100")
-    print("i still love mum")
-elif age == 88: 
-    print("i think im ready to go")
+def f(x, y, z):
+    return x + y + z
+
+result = f(1, 2,3)
+print(result)
+
+
+
+
